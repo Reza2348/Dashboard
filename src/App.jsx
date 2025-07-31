092d6aaf9780427ca86ef8d4e4ced89af175bc4e
Amier Reza majidaie <146933133+Reza2348@users.noreply.github.com>
2025-07-31 19:26:25 +0330

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,21 +5,17 @@ import Sidebar from "./components/Sidebar";
 
 function App() {
   return (
-    <div className="flex flex-col h-screen">
+    <div>
       {/* هدر با ارتفاع ثابت */}
       <Header />
 
       {/* بخش محتوا: سایدبار + بخش اصلی */}
-      <div className="flex flex-1">
+      <div className="flex">
         {/* سایدبار با عرض ثابت */}
-        <div className="w-64 border-r border-gray-200">
-          <Sidebar />
-        </div>
+        <Sidebar />
 
         {/* بخش اصلی با اسکرول عمودی */}
-        <div className="flex-1 overflow-y-auto">
-          <HeroSection />
-        </div>
+        <HeroSection />
       </div>
     </div>
   );
